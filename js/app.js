e5fb909a68903697346964275fe9ae7ee23cb63a
Brown <broemik@dunwoody.edu>
2023-11-17 18:25:46 -0600

    Failed JS changes.

--- js/app.js
+++ js/app.js
@@ -1,40 +1,41 @@
+
 // Variables
 
-let firstName = document.getElementById('firstName');
-let childCheckbox = document.getElementById('childCheckbox');
-let wheelchairSelect = document.getElementById('wheelchairSelect');
-let pickupDatetime = document.getElementById('pickupDatetime');
-let dropoffDatetime = document.getElementById('dropoffDatetime');
-let perferredContactMethod = document.getElementById('perferredContactMethod');
+const firstName = document.getElementById('firstName');
+const childCheckbox = document.getElementById('childCheckbox');
+const wheelchairSelect = document.getElementById('wheelchairSelect');
+const pickupDatetime = document.getElementById('pickupDatetime');
+const dropoffDatetime = document.getElementById('dropoffDatetime');
+const perferredContactMethod = document.getElementById('perferredContactMethod');
 
 // Reservation processing and comfirmation
+// function reservation(){
+//     childCheckbox();
+// 	wheelchairSelect()
+// };
+
+// How you might get the date-time
 
 function reservation(){
-    var msgDiv = document.getElementById("msg");
-    msgDiv.innerText = `Thanks for your reservation, ${firstName}! Your ${childCheckbox} ${wheelchairSelect}. Your pickup time is scheduled for ${pickupDatetime}. Your dropoff time is scheduled for ${dropoffDatetime}. If /you need to change your reservation at any time, please contact us as soon as possible. A email will be sent to you with your reservation comfirmation along with more imformation. Any futher contact information will be sent to you via ${perferredContactMethod}`;
+	
+    msgDiv.innerText = `Thanks for your reservation, ${firstName.value}! Your ${childCheckbox()} ${wheelchairSelect()}. Your pickup time is scheduled for ${pickupDatetime()}. Your dropoff time is scheduled for ${dropoffDatetime()}. If /you need to change your reservation at any time, please contact us as soon as possible. A email will be sent to you with your reservation comfirmation along with more imformation. Any futher contact information will be sent to you via ${perferredContactMethod()}`;
 };
 
 // Kid or adult boolean checkbox
 
-document.addEventListener('DOMContentLoaded', function() {
-    const checkbox = document.getElementById('child-checkbox');
-    const statusElement = document.getElementById('status');
-
-    checkbox.addEventListener('change', function() {
-        if (checkbox.checked) {
-            statusElement.checked = 'Child';
-        }
-        else {
-            statusElement.textContent = 'Adult';
-        }
-    });
-});
+// Kid or adult boolean checkbox
+function childCheckbox() {
+	if (childCheckbox.checked===true) {
+		return "Child";
+	}
+	else {
+		return "Adult";
+	}
+}
 
 // Wheelchair switch dropdown
 function wheelchairSelect(){
-    const selector = document.getElementById('wheelchairtype-menu');
-    const resultElement = document.getElementById('reservationComfirmation');
-    const selectedValue = selector.value;
+    const selector = wheelchairSelect.value;
     
     switch (selector.checked) {
         case 'standard-push':
@@ -72,4 +73,4 @@ function wheelchairSelect(){
 
 // Call onclick function
 
-document.getElementById("submit__btn"),addEventListener("click", reservation); 
+document.getElementById("submit__btn").addEventListener("click", reservation); 
