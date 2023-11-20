647a596728a285e3cc01acc27e0671ec4f2759e9
Brown <broemik@dunwoody.edu>
2023-11-19 19:04:24 -0600

    JS form input works.

--- js/app.js
+++ js/app.js
@@ -1,70 +1,54 @@
 // Variables
-
 const firstName = document.getElementById('firstName');
-const childCheckbox = document.getElementById('childCheckbox');
-const wheelchairSelect = document.getElementById('wheelchairSelect');
+const checkboxStatus = document.getElementById('childCheckbox');
+const wheelchairDropdown = document.getElementById('wheelchairSelect');
 const pickupDatetime = document.getElementById('pickupDatetime');
 const dropoffDatetime = document.getElementById('dropoffDatetime'); 
-const contactMethod = document.getElementsByName('contactMethod');
+const contactMethodInputs = document.getElementsByName('contactMethod');
 const msgDiv = document.getElementById('msg');
 const statusElement = document.getElementById('status');
 const selector = document.getElementById('wheelchairtype-menu');
 const resultElement = document.getElementById('reservationComfirmation');
 const selectedValue = selector.value;
 
-
-// Reservation processing and comfirmation
-
-function test(){
-    msgDiv.innerText = `Thanks for your reservation, ${firstNameValue}! Your ${childCheckbox} ${wheelchairSelect}. Your pickup time is scheduled for ${pickupDatetimeValue}. Your dropoff time is scheduled for ${dropoffDatetimeValue}. If /you need to change your reservation at any time, please contact us as soon as possible. A email will be sent to you with your reservation comfirmation along with more imformation. Any futher contact information will be sent to you via ${perferredContactMethod}.`;
-};
+// Reservation processing and confirmation
+function reservationConfirmation() {
+    msgDiv.innerText = `Thanks for your reservation, ${firstName.value}! Your ${getCheckboxStatus()} ${wheelchairDropdownValue()}. Your pickup time is scheduled for ${pickupDatetime.value}. Your dropoff time is scheduled for ${dropoffDatetime.value}. If you need to change your reservation at any time, please contact us as soon as possible. An email will be sent to you with your reservation confirmation along with more information. Any further contact information will be sent to you via ${getContactMethod()}.`;
+}
 
 // Kid or adult boolean checkbox
-
-function checkboxStatus(){
-    if (childCheckbox.checked===true) {
-        return "child";
-    }
-    else{
-        return "adult";
-    }
-};
+function getCheckboxStatus() {
+    return checkboxStatus.checked ? "child" : "adult";
+}
 
 // Wheelchair switch dropdown
-function wheelchairDropdown(){
+function wheelchairDropdownValue() {
     switch (selector.value) {
         case 'standard-push':
-            resultElement.textContent = 'Standard Push';
-            break;
+            return 'Standard Push';
         case 'light-weight-manual':
-            resultElement.textContent = 'Light weight Manual';
-            break;
+            return 'Light weight Manual';
         case 'stroller':
-            resultElement.textContent = 'Stroller';
-            break;
+            return 'Stroller';
         case 'all-terrain-power':
-            resultElement.textContent = 'All Terrain Power';
-            break;
+            return 'All Terrain Power';
         case 'light-weight-power':
-            resultElement.textContent = 'Light Weight Power';
-            break;
-    };
-};
+            return 'Light Weight Power';
+        default:
+            return 'Unknown Wheelchair Type';
+    }
+}
 
 // Contact radio buttons
-
-function perferredContactMethod(){
-    for (var i = 0; i < contactMethod.length; i++) {
-        if (contactMethod[i].checked) {
-            alert(contactMethod[i].value);
-            return;
+function getContactMethod() {
+    for (var i = 0; i < contactMethodInputs.length; i++) {
+        if (contactMethodInputs[i].checked) {
+            return contactMethodInputs[i].value;
         }
     }
-};
+}
 
 // Call onclick function
-
-
 document.addEventListener("DOMContentLoaded", function() {
-    document.getElementById("btn").addEventListener("click", test);
+    document.getElementById("btn").addEventListener("click", reservationConfirmation);
 });
